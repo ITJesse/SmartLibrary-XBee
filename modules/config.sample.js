837f28648022f333b67dab879bd2259874366b6b
itjesse <jesse@itjesse.cn>
2015-05-19 17:58:01 +0800

    修复少量bug

--- modules/config.sample.js
+++ modules/config.sample.js
@@ -1,5 +1,6 @@
 module.exports = {
     host: 'http://library.itjesse.cn',
+    camera: 'http://B305995.kaicong.info',
     serial: '/dev/cu.usbserial',
     baudrate: 9600
 };
--- server.js
+++ server.js
@@ -39,25 +39,33 @@ var getVal = function() {
 var getRaspi = function() {
     var task = [];
 
-    var file = fs.readFileSync("/sys/class/thermal/thermal_zone0/temp", "utf8");
-    var temp = (parseFloat(file) / 1000).toFixed(1);
-    var tempJson = {
-        mac: 'E84E061C',
-        type: '1',
-        value: temp
-    };
-    task.push(tempJson);
-    console.log("CPU: " + temp);
+    fs.exists("/sys/class/thermal/thermal_zone0/temp", function(exists){
+        if(exists){
+            var file = fs.readFileSync("/sys/class/thermal/thermal_zone0/temp", "utf8");
+            var temp = (parseFloat(file) / 1000).toFixed(1);
+            var tempJson = {
+                mac: 'E84E061C',
+                type: '1',
+                value: temp
+            };
+            task.push(tempJson);
+            console.log("CPU: " + temp);
+        }
+    });
 
-    var file = fs.readFileSync("/proc/loadavg", "utf8");
-    var load = parseFloat(file.slice(0, 4));
-    var loadJson = {
-        mac: 'E84E061C',
-        type: '13',
-        value: load
-    };
-    task.push(loadJson);
-    console.log("LOAD: " + load);
+    fs.exists("/proc/loadavg", function(exists){
+        if(exists){
+            var file = fs.readFileSync("/proc/loadavg", "utf8");
+            var load = parseFloat(file.slice(0, 4));
+            var loadJson = {
+                mac: 'E84E061C',
+                type: '13',
+                value: load
+            };
+            task.push(loadJson);
+            console.log("LOAD: " + load);
+        }
+    });
 
     var mem = Math.floor((os.totalmem() - os.freemem()) / 1024 / 1024);
     var memJson = {
@@ -138,6 +146,46 @@ socket.on('data', function(data){
                 };
                 xbeeList.push(node);
             }
+            break;
+        case "201":
+            request.get('http://192.168.2.117/decoder_control.cgi?command=2', function(error, response, body){
+                console.log(body);
+            }).auth('admin', '123456', true);
+            setTimeout(function(){
+                request.get('http://192.168.2.117/decoder_control.cgi?command=1', function(error, response, body){
+                    console.log(body);
+                }).auth('admin', '123456', true);
+            }, 1000);
+            break;
+        case "202":
+            request.get('http://192.168.2.117/decoder_control.cgi?command=0', function(error, response, body){
+                console.log(body);
+            }).auth('admin', '123456', true);
+            setTimeout(function(){
+                request.get('http://192.168.2.117/decoder_control.cgi?command=1', function(error, response, body){
+                    console.log(body);
+                }).auth('admin', '123456', true);
+            }, 1000);
+            break;
+        case "203":
+            request.get('http://192.168.2.117/decoder_control.cgi?command=6', function(error, response, body){
+                console.log(body);
+            }).auth('admin', '123456', true);
+            setTimeout(function(){
+                request.get('http://192.168.2.117/decoder_control.cgi?command=1', function(error, response, body){
+                    console.log(body);
+                }).auth('admin', '123456', true);
+            }, 1000);
+            break;
+        case "204":
+            request.get('http://192.168.2.117/decoder_control.cgi?command=4', function(error, response, body){
+                console.log(body);
+            }).auth('admin', '123456', true);
+            setTimeout(function(){
+                request.get('http://192.168.2.117/decoder_control.cgi?command=1', function(error, response, body){
+                    console.log(body);
+                }).auth('admin', '123456', true);
+            }, 1000);
             break;
         default:
             setTimeout(function(){
